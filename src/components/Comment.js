a14a8d7ea4377315b085764bd3b3aba4a1d9492f
Archibald Macharia Gacheru <archibald.gacheru@student.moringaschool.com>
2023-09-05 21:53:30 +0300

    Props basics

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Comment(props) {
-  return <div className="comment">{props.commentText}</div>;
+  return (
+  <div className="comment">
+  {props.commentText}
+  </div>
+  );
 }
 
 export default Comment;
